9b7d4c4b628cb7ee8dbd932f670d8ed261851f20
May <baermay98@gmail.com>
2023-11-22 11:07:16 +0100

    Create backup_db.sh

---
+++ backup_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if all required parameters are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <connection_string> <database_name> <backup_path>"
+    exit 1
+fi
+
+# Assign command line arguments to variables
+CONNECTIONURI="$1"
+DBNAME="$2"
+BACKUPPATH="$3"
+
+# Docker command using command line arguments
+mongodump --uri="${CONNECTIONURI}/${DBNAME}?authSource=admin" --gzip --out $BACKUPPATH
